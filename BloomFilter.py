da70d0259834be52d4d598f5ca14ad3fd3d74eb8
김영재 <byeongal@kookmin.ac.kr>
2020-08-12 17:39:40 +0900

    bin

--- BloomFilter.py
+++ BloomFilter.py
@@ -1,14 +1,13 @@
 import math
 import hashlib
 
-
 class BloomFilter:
     def __init__(self, number_of_elements=100, fp_prob=0.01):
         self.filter_size = self.__calculate_filter_size(number_of_elements, fp_prob)
-        self.__mod_value = (1 << self.filter_size) - 1
+        self.bins = [ 0 ] * (int(self.filter_size / 64) + 1)
         self.number_of_hashes = self.__calculate_number_of_hashes(self.filter_size, number_of_elements)
-        self.__slice = math.ceil(math.log2(self.filter_size))
-        self.filter = 0
+        self.slice = math.ceil(math.log2(self.filter_size))
+        self.mod_value = (1 << self.slice) - 1
 
     @classmethod
     def __calculate_filter_size(cls, number_of_elements, fp_prob):
@@ -22,21 +21,28 @@ class BloomFilter:
         i = 0
         while i < self.number_of_hashes:
             idx = int(hashlib.blake2b(data + '{}'.format(i).encode()).hexdigest(), 16)
-            for j in range(0, 512, self.__slice):
-                temp = idx & self.__mod_value
-                self.filter |= (temp % self.filter_size)
+            for j in range(0, 512, self.slice):
+                temp = idx & self.mod_value
+                if temp >= self.filter_size:
+                    temp -= self.filter_size
+                bin_idx, bit_idx = divmod(temp, 64)
+                self.bins[bin_idx] |= (1 << bit_idx)
                 i += 1
-                idx >>= self.__slice
+                idx >>= self.slice
 
     def check(self, data):
         i = 0
         while i < self.number_of_hashes:
             idx = int(hashlib.blake2b(data + '{}'.format(i).encode()).hexdigest(), 16)
-            for j in range(0, 512, self.__slice):
-                temp = idx & self.__mod_value
-                self.filter |= (temp % self.filter_size)
+            for j in range(0, 512, self.slice):
+                temp = idx & self.mod_value
+                if temp >= self.filter_size:
+                    temp -= self.filter_size
+                bin_idx, bit_idx = divmod(temp, 64)
+                if (self.bins[bin_idx] >> bit_idx) & 1 == 0:
+                    return False
                 i += 1
-                idx >>= self.__slice
+                idx >>= self.slice
         return True
 
     def __str__(self):
