9b27632d919b14f1492883430f037bd3cafa5d9c
sonhyeongi <forever2331@koomin.ac.kr>
2020-08-12 15:36:56 +0900

    add calculate_number_of_hashes function

--- BloomFilter.py
+++ BloomFilter.py
@@ -5,12 +5,16 @@ import math
 class BloomFilter:
     def __init__(self, number_of_elements=100, fp_prob=0.01):
         self.filter_size = self.__calculate_filter_size(number_of_elements, fp_prob)
-        self.number_of_hashes = None
+        self.number_of_hashes = self.__calculate_number_of_hashes(self.filter_size, number_of_elements)
         self.filter = (1 << self.filter_size)
 
     @classmethod
     def __calculate_filter_size(cls, number_of_elements, fp_prob):
         return int(-number_of_elements * math.log(fp_prob) / math.pow(math.log(2), 2))
+
+    @classmethod
+    def __calculate_number_of_hashes(cls, filter_size, number_of_elements):
+        return int(math.ceil((filter_size / number_of_elements) * math.log(2)))
 
     def add(self, data):
         for i in range(self.number_of_hashes):
