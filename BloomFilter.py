56f8eacfac294e2037ccd03aa0ccab0ac1749aa8
sonhyeongi <forever2331@koomin.ac.kr>
2020-08-12 15:34:23 +0900

    add calculate_filter_size function

--- BloomFilter.py
+++ BloomFilter.py
@@ -1,10 +1,16 @@
 import mmh3
+import math
+
 
 class BloomFilter:
-    def __init__(self, number_of_elements = 100, fp_prob = 0.01):
-        self.filter_size = None
+    def __init__(self, number_of_elements=100, fp_prob=0.01):
+        self.filter_size = self.__calculate_filter_size(number_of_elements, fp_prob)
         self.number_of_hashes = None
         self.filter = (1 << self.filter_size)
+
+    @classmethod
+    def __calculate_filter_size(cls, number_of_elements, fp_prob):
+        return int(-number_of_elements * math.log(fp_prob) / math.pow(math.log(2), 2))
 
     def add(self, data):
         for i in range(self.number_of_hashes):
